a8a36d23f4651382f4d506d8ee56c53301669de0
Chere <alexandre2405@gmail.com>
2024-09-07 19:00:48 -0300

    :sparkles: feat(ProductDetails): Adiciona exibição de detalhes do produto e carregamento condicional

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
+import styleHome from '../../styles/Home.module.css'
+import Image from "next/image";
 
 interface Product {
     id: number,
@@ -16,8 +18,8 @@ interface Product {
   }
 
 const ProductDetails: React.FC = () => {
-    const router = useRouter();;
-    const { id } = router.query;;
+    const router = useRouter();
+    const { id } = router.query;
     const [product, setProduct] = useState<Product | null>(null);
 
     useEffect(() => {
@@ -27,4 +29,18 @@ const ProductDetails: React.FC = () => {
             .catch(e => console.error('Error fetching product:', e))
         }
     }, [id])
+
+    if (!product) return <div>Loading...</div>;
+    
+    return (
+        <div className={styleHome.container}>
+            <h1>{product.title}</h1>
+            <Image src={product.image} alt={product.title} width={300} height={300}/>
+            <p>Preço: {product.price}</p>
+            <p>Descrição: {product.description}</p>
+            <p>Categoria: {product.category}</p>
+        </div>
+    )
 }
+
+export default ProductDetails
