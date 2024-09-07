f06b8186ab025e25e9c3869deb65d9cd18f45fd1
Chere <alexandre2405@gmail.com>
2024-09-07 19:59:15 -0300

    :sparkles: feat(ProductDetails): Adiciona funcionalidade de pesquisa e filtro por categoria

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,8 @@ interface Products {
 
 const Home: React.FC = () => {
   const [products, setProducts] = useState<Products[]>([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isFilteredProducts, setIsFilteredProducts] = useState<Products[]>(products)
 
   useEffect(() => {
     axios.get<Products[]>('https://fakestoreapi.com/products')
@@ -25,12 +27,47 @@ const Home: React.FC = () => {
     .catch(error => console.error('Error fetching products:', error));
   }, [])
 
+  useEffect(() => {
+    const searchResults = products.filter(product => {
+      return product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+
+    setIsFilteredProducts(searchResults)
+  }, [searchTerm, products])
+
+  function filterCategory(category: string){
+    const filtered = products.filter( product => 
+       product.category.toLowerCase() === category.toLowerCase()
+    );
+
+    setIsFilteredProducts(filtered);
+    setSearchTerm('');
+  }
+
+  function allProducts(){
+    setIsFilteredProducts(products)
+  }
+
   return (
     <div className={styleHome.container}>
       <h1>Product List</h1>
 
+      <input 
+      type="text" 
+      placeholder="Pesquisar produtos"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <button onClick={() => allProducts()}>Todos</button>
+      <button onClick={() => filterCategory("men's clothing")}>Men</button>
+      <button onClick={() => filterCategory("women's clothing")}>Women</button>
+      <button onClick={() => filterCategory("electronics")}>Electronics</button>
+      <button onClick={() => filterCategory("jewelery")}>Jewelry</button>
+      
+
       <div className={styleHome.grid}>
-        {products.map(product => (
+        {isFilteredProducts.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
